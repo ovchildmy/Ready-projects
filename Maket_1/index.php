7b078333005191a06bcef14c963e21945fbc6c68
Ilya <ovchildmy@gmail.com>
2018-07-04 02:00:04 +0300

    Maket_1 is ready!

---
+++ Maket_1/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style/uikit-rtl.css">
+	<link rel="stylesheet" type="text/css" href="style/uikit.css">
+	<link rel="stylesheet" type="text/css" href="style/core.css">
+	<link rel="stylesheet" type="text/css" href="style/header.css">
+	<link rel="stylesheet" type="text/css" href="style/navigation.css">
+	<link rel="stylesheet" type="text/css" href="style/content.css">
+	<link rel="stylesheet" type="text/css" href="style/shortNews.css">
+	<link rel="stylesheet" type="text/css" href="style/events.css">
+	<link rel="stylesheet" type="text/css" href="style/footer.css">
+	<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+	<title>Maket_1</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<div class="bodyBg-left"></div>
+<div class="news-left"></div>
+
+<div class="container">
+	<div class="bodyBg-right"></div>
+
+
+	<div class="header">
+		<div class="headerImage">
+			<img src="img/mainImg.jpg">
+		</div>
+		<div class="headerNav">
+			<div><span>73</span></div>
+			<span class="header-bus"><i class="fa fa-bus" aria-hidden="true"></i><span>Автобусы</span></span>
+			<span class="header-canteen"><i class="fa fa-cutlery" aria-hidden="true"></i><span>Столовая</span></span>
+			<span class="header-floors"><i class="fa fa-database" aria-hidden="true"></i><span>Этажи</span></span>
+			<span class="header-places"><i class="fa fa-map-marker" aria-hidden="true"></i><span>Территории</span></span>
+			<span class="header-search"><i class="fa fa-search" aria-hidden="true"></i> <span>Поиск</span></span>
+		</div>
+		<div class="headerUser">
+			<span><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+			<div class="userMenu"><span></span><span>Александр</span></div>
+		</div>
+	</div>
+
+	<div class="navigation">
+		<div>Наша компания</div>
+		<div>Наша команда</div>
+		<div>Моя <div>безопасность</div></div>
+		<div>Управляющая компания</div>
+		<div>Наши предприятия</div>
+		<div>Справочники</div>
+		<div>Сервисы</div>
+	</div>
+
+
+	<div class="content">
+		<div class="news-container">
+			<div class="news-top">
+				<div class="news-top_photo"></div>
+				<div class="news-top_text">
+					<div>Евраз Тв</div>
+					<div>65-летие Рельсобалочного цеха ЕВРАЗ <span>НТМК</span></div>
+				</div>
+			</div>
+			<div class="news-bottom">
+				<div class="news-right"></div>
+				<div class="news-bottom_headline">Иванов <span>Алексей</span></div>
+				<div class="news-bottom_content">Мы все хотим, чтобы ЕВРАЗ был эффективной компанией с высоким уровнем безопасности труда, ориентированной на запросы клиентов, поставляющей продукцию высокого качества...</div>
+				<div class="news-bottom_footer">Старший Вице-президент, <br>Блок коммерции и развития бизнеса</div>
+			</div>
+		</div>
+
+		<div class="arrows">
+			<div class="arrowLeft"><</div>
+			<div class="arrowRight">></div>
+		</div>
+
+		<div class="shortNews">
+			<div class="shortNews-user">
+				<div class="shortNews-user_photo"></div>
+				<div>
+					<div class="shortNews-user_name">Царенко Владимир</div>
+					<div class="shortNews-user_position">Главный специалист ЕвразХолдинг</div>
+				</div>
+			</div>
+			<div class="shortNews-warning">
+				<div class="shortNews-warning_photo"></div>
+				<div class="shortNews-warning_date">12 декабря</div>
+				<div class="shortNews-warning_message">Встреча с представителями банка ВТБ24</div>
+			</div>
+			<div class="shortNews-event">
+				<div class="shortNews-event_photo"></div>
+				<div class="shortNews-event_date">13 января</div>
+				<div class="shortNews-event_message">О проведении в 2017 году соревнований по горным лыжам и сноуборду имени Андрея Севенюка</div>
+			</div>
+			<div class="shortNews-nav">
+				<div class="shortNews-nav_point"></div>
+				<div class="shortNews-nav_point"></div>
+				<div class="shortNews-nav_point"></div>
+				<div class="shortNews-nav_point"></div>
+				<div class="shortNews-nav_point"></div>
+			</div>
+		</div>
+
+		<div class="events">
+			<div class="lastEvents">
+				<div class="event">
+					<div class="event-photo">
+						<div class="event-marker"><span class="event-marker__south">Юг</span></div>
+					</div>
+					<div class="event-name">ЕВРАЗ передал здание шахты &#171;МУК-96&#187; Междуреченску</div>
+					<div class="event-date">Сегодня</div>
+				</div>
+				<div class="event">
+					<div class="event-photo">
+						<div class="event-marker"><span class="event-marker__south">Юг</span></div>
+					</div>
+					<div class="event-name">На теплоэлектроцентрали <br> ЕВРАЗ Южкокса внедрили систему LOTO</div>
+					<div class="event-date"></div>
+				</div>
+				<div class="event">
+					<div class="event-photo">
+						<div class="event-marker event-marker_red">Зсмк</div>
+					</div>
+					<div class="event-name">ЕВРАЗ ЗСМК посетили представители ключевого дилера строительного металлопроката</div>
+					<div class="event-date">Вчера</div>
+				</div>
+				<div class="event">
+					<div class="event-photo">
+						<div class="event-marker">Нтмк</div>
+					</div>
+					<div class="event-name">ЕВРАЗ НТМК повышает эффективность выплавки <br>стали</div>
+					<div class="event-date">19 декабря</div>
+				</div>
+			</div>
+
+			<div class="events-other">
+				<div class="enents-news">
+					<div></div>
+					<div>Все новости</div>
+				</div>
+				<div class="events-region">
+					<div></div>
+					<div>Мой регион</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="footer">
+			<div class="footer-firm">
+				<img src="img/mainImg.jpg">
+				<div>Мы ценим своих сотрудников и создаем возможности для их профессионального развития и карьерного роста</div>
+			</div>
+			<div class="footer-contacts">
+				<div class="footer-contacts_headline">Горячая линия</div>
+				<div class="call">
+					<div class="call-number">8-800-555-88-88</div>
+					<div class="call-name">для сотрудников российских предприятий</div>
+				</div>
+				<div class="call">
+					<div class="call-number">0-888-502-118</div>
+					<div class="call-name">для сотрудников украинских предприятий</div>
+				</div>
+				<div class="call">
+					<div class="call-number">8-800-080-43-58</div>
+					<div class="call-name">для сотрудников ЕВРАЗ Каспиан Сталь</div>
+				</div>
+				<div class="call-mail">vopros@evraz.com</div>
+				<div class="hotLine">Написать в горячую линию</div>
+			</div>
+			<div class="footer-market">
+				<div class="footer-market_headline">
+					<div>Биржевые котировки</div>
+					<div class="footer-market_date">28 декабря 12:35 (GMT)</div>
+				</div>
+
+				<div class="footer-market_prices">
+					<div class="footer-market_prices--left">
+						<div class="footer-price_name">EVRAZ (LSE)</div>
+						<div class="footer-price_price"><span>225. 10</span> GBX</div>
+						<div class="footer-price_pos">1.20 (0.54 %)	&#160; <span class="triangle">&#9650;</span></div>
+					</div>
+					<div class="footer-market_prices--right">
+						<div>Открытие <span>221, 00</span></div>
+						<div>Максимум <span>238, 80</span></div>
+						<div>Минимум <span>221, 00</span></div>
+					</div>
+				</div>
+
+				<div class="footer-market_prices">
+					<div class="footer-market_prices--left">
+						<div class="footer-price_name">Raspadskaya (MCX)</div>
+						<div class="footer-price_price"><span>78. 96</span> RUB</div>
+						<div class="footer-price_pos">0.65 (0.83 %)	&#160; <span class="triangle">&#9650;</span></div>
+					</div>
+					<div class="footer-market_prices--right">
+						<div>Открытие <span>221, 00</span></div>
+						<div>Максимум <span>238, 80</span></div>
+						<div>Минимум <span>221, 00</span></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="footer-other">
+				<div class="footer-other_headerline">Портал</div>
+				<div class="footer-other_link">Обратная связь</div>
+				<div class="footer-other_link">Карта разделов портала</div>
+				<div class="footer-other_link">Заявка в Service Desk</div>
+				<div class="footer-other_link">Помощь</div>
+				<div class="footer-other_link">Команда</div>
+
+				<div class="footer-other_message">
+					Если вы нашли ошибку, выделите её курсором, нажмите одновременно клавиши &#171;CTRL&#187; и &#171;ENTER&#187; и мы скорректируем неточность.
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+
+
+
+
+</div>
+
+	<script type="text/javascript" src="lib/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="lib/uikit.js"></script>
+	<script type="text/javascript" src="lib/uikit-icons	.js"></script>
+	<script type="text/javascript" src="lib/core.js"></script>
+</body>
+</html>
